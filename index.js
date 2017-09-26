498acda61e0076074733ef2df707de8e34a38cb8
vimrus <vimrus.com@gmail.com>
2017-09-27 01:12:13 +0800

    * fix.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ hubble.getXML('http://finance.caixin.com/finance_news.xml', function (error, res
 	$('item').each(function (index, value) {
 
 		var url = $(this).find('link').text();
-		var key = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.html'));
+		var id  = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.html'));
 		var dom = $(this);
 
-		articles.get('key', key, function (article) {
+		articles.get('id', id, function (article) {
 			if (article) {
 				return;
 			}
@@ -17,7 +17,7 @@ hubble.getXML('http://finance.caixin.com/finance_news.xml', function (error, res
 			var summary = content.replace(/<\/?[^>]*>/g,'').trim().substring(0, 50);
 
 			var article = {
-				key: key,
+				id: id,
 				title: title,
 				content: content,
 				summary: summary,
